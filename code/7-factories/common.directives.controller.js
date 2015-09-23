4e8644b11ccd0c75351c341890f90e03d83e3b7d
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-09-23 14:09:44 +0200

    Introduces factories and minification save di

---
+++ code/7-factories/common.directives.controller.js
@@ -0,0 +1,13 @@
+(function() {
+  angular
+    .module('factories', [])
+    .controller('DirectivesController', Controller);
+
+  Controller.$inject = ['trainees'];
+
+  // Konstruktorfunktion
+  function Controller(a) {
+
+    this.people = a.getAll();
+  }
+}());
