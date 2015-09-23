33f8b4c4469afe952008a970b9e8768a56309d1b
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-09-23 15:44:36 +0200

    Loads all trips initially

--- code/uebung-trips/trips.controller.js
+++ code/uebung-trips/trips.controller.js
@@ -5,13 +5,15 @@
    .module('trips')
    .controller('tripsController', tripsController);
 
-   tripsController.$inject = ['tripsService'];
+   tripsController.$inject = ['tripsService', 'tripsFactory'];
 
-  function tripsController(tripsService) {
+  function tripsController(tripsService, tripsFactory) {
     var vm = this;
 
     vm.entries = [];
     vm.getTrips = getTrips;
+
+    vm.entries = tripsFactory.getAll();
 
     function getTrips(country) {
       vm.entries = tripsService.getTripsOfCountry(country);
