431f81146edfdd6b45910b1b7c351cab31469fbc
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-09-24 09:59:31 +0200

    Introduces core and app module structuring the app

--- code/9-routing/components/core/core.module.js
+++ code/9-routing/components/core/core.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-   .module('trips', [
-     'core'
+   .module('core', [
+     'ngRoute'
    ]);
 }());
--- code/9-routing/components/welcome/welcome.module.js
+++ code/9-routing/components/welcome/welcome.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-   .module('trips', [
+   .module('welcome', [
      'core'
    ]);
 }());
