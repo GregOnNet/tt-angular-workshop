1323718fb6affd3a09cec3cb889418c1318c0ef2
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-09-24 10:45:44 +0200

    Uses $filter inside controller

--- code/10-filter/join.controller.js
+++ code/10-filter/join.controller.js
@@ -5,9 +5,16 @@
    .module('filterDemo')
    .controller('filterController', FilterController);
 
-  function FilterController() {
+  FilterController.$inject = ['$filter'];
+
+  function FilterController($filter) {
     var vm = this;
 
+    vm.sorted = [];
+    vm.joined = '';
     vm.women = ['Jaqueline', 'Chantalle', 'Nadine', 'Charlotte'];
+
+    vm.sorted = $filter('orderBy')(vm.women);
+    vm.joined = $filter('join')(vm.sorted, '; ');
   }
 }());
